1cea7e5038dde05df23808d7b111b85443619eb9
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-11-23 16:35:08 +0300

    added user update controller

--- projectServer/Routers/controllers/users.js
+++ projectServer/Routers/controllers/users.js
@@ -30,4 +30,37 @@ const getAllUsers = (req, res) => {
     });
 };
 
-module.exports = { createUser, getAllUsers };
+const updateUser = (req, res) => {
+  /*shahad you will use this for the update inputs in the user page,
+   the input values will check this function*/
+  const { userName } = req.params;
+  const { userName, email, password } = req.body;
+  let index = 0;
+  try {
+    const found = userModel.find((item, i) => {
+      index = i;
+      return item.userName == userName;
+    });
+    if (found) {
+      if (found.userName == "guest") {
+        res.status(404).json("you are a guest, sign up to get update option");
+      } else {
+        if (userName) found.userName = userName;
+        if (email) found.email = email;
+        if (password) found.password = password;
+
+        userModel[index] = found;
+        res.status(200).json(found);
+      }
+    } else res.status(400).json("user not found");
+  } catch (error) {
+    res.status(400).json(error.meassage);
+  }
+};
+
+// const deleteUser = (req, res) => { //figure this one out later
+//   userModel.remove({ userName: req.params }).then((result) => {
+//     res.send()
+//   })};
+
+module.exports = { createUser, getAllUsers, updateUser };
