bae801b09afc7b7f3375bad03832de1045c007d3
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-11-23 16:37:10 +0300

    added update user route

--- projectServer/Routers/routes/users.js
+++ projectServer/Routers/routes/users.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { createUser, getAllUsers } = require("./../controllers/users");
+const {
+  createUser,
+  getAllUsers,
+  updateUser,
+} = require("./../controllers/users");
 
 const userRouter = express.Router();
 
 userRouter.post("/user", createUser);
 userRouter.get("/user", getAllUsers);
+userRouter.put("/user", updateUser);
 
 module.exports = userRouter;
