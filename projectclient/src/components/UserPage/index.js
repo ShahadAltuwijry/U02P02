81cd6b4c50669ff2ba2c43fd2239a6730c13955d
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-11-24 23:51:10 +0300

    added a guest message

--- projectclient/src/components/UserPage/index.js
+++ projectclient/src/components/UserPage/index.js
@@ -1,9 +1,27 @@
 import React from "react";
 import Footer from "./../Footer";
+import "./style.css";
+
 const UserPage = () => {
   return (
-    <div>
-      <h1>User page</h1>
+    <div className="userDiv">
+      <div className="guestDiv">
+        <h1 className="guestHead">
+          :( ضيفنا الغالي، لا نملك معلومات كافية عنك لاظهار هذه الصفحة لك
+        </h1>
+        <br />
+        <h3> !ابدأ رحلتك الممتعة معنا في السعودية عن طريق التسجيل </h3>
+        <br />
+        <a href="./signUp" className="anchorg">
+          سجل الأن
+        </a>
+        <br />
+        <h3>او اذا كان عندك حساب </h3>
+        <br />
+        <a href="./signin" className="anchorg">
+          سجل دخولك
+        </a>
+      </div>
       <Footer />
     </div>
   );
