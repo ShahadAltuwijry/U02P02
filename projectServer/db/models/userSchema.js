ad42857f0d86f10a8e34a3731c9eedb420f56e16
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-11-25 10:50:35 +0300

    added relation

--- projectServer/db/models/userSchema.js
+++ projectServer/db/models/userSchema.js
@@ -4,7 +4,10 @@ const userSchema = new mongoose.Schema({
   userName: { type: String, trim: true, required: true, unique: true },
   email: { type: String, trim: true, required: true, unique: true },
   password: { type: Number, trim: true, required: true },
-  visits: { type: Array }, //child from program&spots
+  visits: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "spots" },
+    { type: mongoose.Schema.Types.ObjectId, ref: "packages" },
+  ], //child from packages&spots
 });
 
 module.exports = mongoose.model("User", userSchema);
